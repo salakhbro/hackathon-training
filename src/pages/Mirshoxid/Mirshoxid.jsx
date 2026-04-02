dc1bd9a3730e3f38514d25313c03ca0069670b5f
fireaspectII <smalldangernight@gmail.com>
2026-04-02 14:45:31 +0500

    mirshohid

--- src/pages/Mirshoxid/Mirshoxid.jsx
+++ src/pages/Mirshoxid/Mirshoxid.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function Mirshoxid() {
+  const boxStyle = {
+    maxWidth: '900px',
+    margin: '30px auto',
+    padding: '25px',
+    borderRadius: '16px',
+    backgroundColor: '#ffffff',
+    boxShadow: '0 10px 30px rgba(0, 0, 0, 0.22)',
+    textShadow: '1px 1px 3px rgba(0, 0, 0, 0.45)',
+    border: '1px solid rgba(0, 0, 0, 0.08)',
+    color: '#222',
+    lineHeight: '1.5',
+  }
+
   return (
-    <div>
-        <h1>Mirshohid page</h1>
+    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={boxStyle}>
+        <h1>Mirshohid Page</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores numquam unde est architecto fugit reiciendis cum facilis perferendis alias voluptas in consequatur facere deserunt qui nostrum, quo maxime voluptatum minima maiores exercitationem molestiae repellat! Nobis assumenda amet obcaecati sequi sapiente. Corrupti, amet? Quos, commodi quas dolor tenetur quae repellat. Laborum!</h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis enim error, itaque provident, voluptatum perferendis minima velit ea officia facere ipsam recusandae adipisci voluptatibus, quae sapiente inventore dignissimos cupiditate sunt fugiat possimus delectus iusto? Ea odio recusandae a itaque veniam impedit accusamus quod qui rem soluta. Dolor recusandae officia modi vero reprehenderit iste praesentium sequi tenetur provident facere laboriosam, odit reiciendis assumenda, delectus corporis nemo vel esse illum! Atque, libero.
+        </h3>
+      </div>
     </div>
   )
 }
